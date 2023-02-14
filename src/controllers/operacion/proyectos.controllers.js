d6d3ebbfb556039ef527eb7f6bbe4b4bd8277d22
Angel Tores <angel_torres2702@outlook.com>
2023-02-14 17:54:03 -0600

    super changes

--- src/controllers/operacion/proyectos.controllers.js
+++ src/controllers/operacion/proyectos.controllers.js
@@ -41,7 +41,7 @@ export const updProyecto = async (req, res) => {
 export const getProductoo = async (req, res) => {
   try {
     const searchText = req.query.searchText;
-    const result = await pool.query(`SELECT * FROM productos WHERE producto_descripcion LIKE '%${searchText}%' AND producto_estatus_baja = 0 LIMIT 15`);
+    const result = await pool.query(`SELECT productos.*, marcas.* FROM productos JOIN marcas ON productos.producto_marca_id = marcas.marca_id WHERE producto_descripcion LIKE '%${searchText}%' AND producto_estatus_baja = 0 LIMIT 15`);
     res.json(result);
   } catch (error) {
     console.error(error);
@@ -63,7 +63,6 @@ export const addServicio = async (req, res) => {
   const maxOrden = await getMaxOrden();
   try {
     const producto =  {
-    producto_codigo: req.body.producto_codigo,
     producto_modelo: req.body.producto_modelo,
     producto_marca_id: req.body.producto_marca_id ,
     producto_unidad_id: req.body.producto_unidad_id ,
@@ -81,7 +80,7 @@ export const addServicio = async (req, res) => {
     producto_descripcion: req.body.producto_descripcion ,
   };
 
-    const result = await pool.query("INSERT INTO productos SET ?", [producto]);
+    await pool.query("INSERT INTO productos SET ?", [producto]);
 
     const productoInsertado = await pool.query("SELECT * FROM `productos` WHERE producto_id = (SELECT MAX(producto_id) FROM productos)");
     
@@ -105,13 +104,13 @@ export const addServicio = async (req, res) => {
       tarjeta_ioficina_porcentaje: "0",
       tarjeta_financiamiento_porcentaje: "0",
     }
-    const insumoInsert = await pool.query("INSERT INTO cotizaciones_insumos SET ?", [insumo]);
-    return res.redirect('/dashboard/operacion/proyectos/editar/'+req.body.insumo_cotizacion_id)
+    await pool.query("INSERT INTO cotizaciones_insumos SET ?", [insumo]);
+    return res.status(200).json({ message: "Producto agregado correctamente" })
   } catch (error) {
     console.log(error);
     res.json({ message: "Error en la consulta, no se puede agregar el servicio" });
   }
-};
+}; 
 
 
 export const delProyecto = async (req, res) => {
@@ -129,6 +128,100 @@ export const delProyecto = async (req, res) => {
     }
   }
 
+  export const updServicio = async (req, res) => {
+    const maxOrden = await getMaxOrden();
+    try {
+      const producto =  {
+        producto_codigo: req.body.producto_codigo,
+        producto_modelo: req.body.producto_modelo,
+        producto_marca_id: req.body.producto_marca_id ,
+        producto_unidad_id: req.body.producto_unidad_id ,
+        producto_tipo_id: req.body.producto_tipo_id ,
+        producto_familia_id: req.body.producto_familia_id ,
+        producto_serie_id: req.body.producto_serie_id ,
+        producto_costo: req.body.producto_costo ,
+        producto_costo_fecha: req.body.producto_costo_fecha ,
+        producto_mo: req.body.producto_mo ,
+        producto_icampo:  req.body.producto_icampo ,
+        producto_ioficina:  req.body.producto_ioficina ,
+        producto_hm: req.body.producto_hm ,
+        producto_financiero: req.body.producto_financiero ,
+        producto_utilidad: req.body.producto_utilidad ,
+        producto_descripcion: req.body.producto_descripcion ,
+    };
+  
+      await pool.query("UPDATE productos SET ? WHERE producto_id = ", [producto]);
+  
+      const productoInsertado = await pool.query("SELECT * FROM `productos` WHERE producto_id = (SELECT MAX(producto_id) FROM productos)");
+      
+      
+      const insumo = {
+        insumo_cotizacion_id: req.body.insumo_cotizacion_id,
+        insumo_cantidad: req.body.cantidad,
+        insumo_producto_id: productoInsertado[0][0].producto_id,
+        insumo_precio_mo: req.body.producto_precio_venta,
+        insumo_orden: maxOrden + 1,
+        tarjeta_ma_costo: "0",
+        tarjeta_mo_jornadas: "0",
+        tarjeta_mo_ayudante_salario: "0",
+        tarjeta_mo_ayudante_jornadas: "0",
+        tarjeta_mo_ingeniero_salario: "0",
+        tarjeta_mo_ingeniero_jornadas: "0",
+        tarjeta_mo_supervisor_salario: "0",
+        tarjeta_mo_supervisor_jornadas: "0",
+        tarjeta_hm_porcentaje: "0",
+        tarjeta_icampo_porcentaje: "0",
+        tarjeta_ioficina_porcentaje: "0",
+        tarjeta_financiamiento_porcentaje: "0",
+      }
+
+      await pool.query("INSERT INTO cotizaciones_insumos SET ?", [insumo]);
+      return res.status(200).json({ message: "Producto agregado correctamente" })
+    } catch (error) {
+      console.log(error);
+      res.json({ message: "Error en la consulta, no se puede agregar el servicio" });
+    }
+  };
+
+  //? add producto
+
+  export const addProducto = async (req, res) => {
+    try {
+      const maxOrden = await getMaxOrden();
+        
+      const insumo = {
+        insumo_cotizacion_id: req.body.cotizacion_id,
+        insumo_cantidad: req.body.cantidad,
+        insumo_producto_id: req.body.producto,
+        insumo_diciplina_id: req.body.disciplina_id,
+        insumo_precio_mo: req.body.mo,
+        insumo_orden: maxOrden + 1,
+        insumo_precio_ma: req.body.ma,
+        tarjeta_mo_jornadas: req.body.tarjeta_producto_mo,
+        tarjeta_hm_porcentaje: req.body.tarjeta_producto_hm,
+        tarjeta_icampo_porcentaje: req.body.tarjeta_producto_icampo,
+        tarjeta_ioficina_porcentaje: req.body.tarjeta_producto_ioficina,
+        tarjeta_financiamiento_porcentaje: req.body.tarjeta_producto_financiero
+      }
+
+      const tarjeta = {
+        tarjeta_cotizacion_id: req.body.cotizacion_id,
+        tarjeta_mo_jornadas: req.body.tarjeta_producto_mo,
+        tarjeta_hm_porcentaje: req.body.tarjeta_producto_hm,
+        tarjeta_icampo_porcentaje: req.body.tarjeta_producto_icampo,
+        tarjeta_ioficina_porcentaje: req.body.tarjeta_producto_ioficina,
+        tarjeta_financiamiento_porcentaje: req.body.tarjeta_producto_financiero
+      }
+
+      await pool.query("INSERT INTO cotizaciones_insumos SET ?", [insumo])
+      await pool.query("INSERT INTO cotizaciones_tarjetas SET ?", [tarjeta])
+
+      return res.status(200).json({ message: "Producto agregado correctamente" })
+    } catch (error) {
+      console.log(error)
+      return res.json({ message: "Error en la consulta, no se puede agregar el servicio" });
+    }
+  }
  
   //! delete insumo
   //* Falta traer el params desde el front porque no lo reconoce
@@ -147,12 +240,12 @@ export const delProyecto = async (req, res) => {
   };
 
 
-
   //? prefactura
   const getIdPre = async () => {
     try {
       const id = await pool.query('SELECT * FROM prefacturas ORDER BY prefactura_id DESC LIMIT 1')
-      
+    
+
       return id[0][0].prefactura_id
     } catch (error) {
       console.log(error)
--- src/routes/operacion.routes.js
+++ src/routes/operacion.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import {addRequisicion, updRequisicion, delRequisicion} from "../controllers/operacion/requisicion.controller.js";
-import {addProyecto, updProyecto, delProyecto, getProductoo, addServicio, deleteInsumo, addPrefacturaOP} from "../controllers/operacion/proyectos.controllers.js";
+import {addProyecto, updProyecto, delProyecto, getProductoo, addServicio, deleteInsumo, addPrefacturaOP, addProducto} from "../controllers/operacion/proyectos.controllers.js";
 import {updAutorizacion, delAutorizacion} from "../controllers/operacion/autorizacion.controller.js";
 import {addFolio, updFolio, delFolio} from "../controllers/operacion/folio.controller.js";
 const rutasOperacion = Router();
@@ -17,10 +17,12 @@ rutasOperacion.post('/updProyecto/:id', updProyecto); //?Actualiza info del proy
 
 rutasOperacion.post('/delProyecto/:id', delProyecto); //?Elimina info del proyecto
 
+//? productos / servicios
 rutasOperacion.get('/getProducto/', getProductoo); //?Obtiene info del proyecto
 
 rutasOperacion.post('/addServicio', addServicio)//? add  servicio cotizacion / proyecto
 rutasOperacion.post('/deleteInsumo/:id', deleteInsumo)//! del  servicio cotizacion / proyecto
+rutasOperacion.post('/addProductToProject', addProducto)//? add producto cotizacion / proyecto
 
 //?Autorizacion
 rutasOperacion.post('/updAutorizacion/:id', updAutorizacion);
